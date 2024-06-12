6bb122aa5a66fad296e9cb99f643a89c020f0ee1
vidurkhanal <vkhanal@caldwell.edu>
2024-06-12 14:19:20 -0400

    chore: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
+    {
+      url: "https://www.evenscribe.com/legal/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.evenscribe.com/legal/tos",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
   ];
 }
